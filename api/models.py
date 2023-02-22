794826ab2700db344e61b0b8e45454e366dd808f
nathanlu0902 <nathan.lu0902@outlook.com>
2023-02-22 23:00:09 +0800

    *

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 GENDER_CHOICES=(
   ('m','male'),
@@ -38,13 +39,23 @@ class Goods(models.Model):
     goodsCategoryId=models.ForeignKey('GoodsCategory',on_delete=models.CASCADE) #分类删除则菜品也删除
     goodsSale=models.IntegerField(default=0)
     goodsPic=models.CharField(max_length=100,blank=True,null=True)
+
+    class Meta:
+       verbose_name="商品信息"
+       verbose_name_plural=verbose_name
     
     def __str__(self):
-      return self.id
+      return self.goodsName
 
 class GoodsCategory(models.Model):
-    category=models.CharField(max_length=30,unique=True)
-    id=models.AutoField(primary_key=True,unique=True)
+    category=models.CharField(max_length=30,unique=True,help_text="类别名")
+    id=models.AutoField(primary_key=True,unique=True,help_text="类别code")
+    desc=models.TextField(help_text="类别描述",null=True)
+    add_time=models.DateTimeField(default=datetime.now)
+
+    class Meta:
+       verbose_name="商品类别"
+       verbose_name_plural=verbose_name
 
     def __str__(self):
       return self.category
